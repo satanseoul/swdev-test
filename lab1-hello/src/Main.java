e9f4c31d8bcac76bfcc31c127e918afa3cbc0b19
satanseoul <puwanai077@gmail.com>
2019-07-02 18:53:46 +0700

    653

---
+++ lab1-hello/src/Main.java
@@ -0,0 +1,30 @@
+
+public class Main {
+	
+	public static void main(String args[])
+	{
+//		int a = 10;
+//		double price = 10.5;
+//		String name = "Satan Seoul";
+//		for(int i=0; i<5; i++)
+//		System.out.println("Hello: " + name);
+//		System.out.println("Price: " + price);
+//		System.out.println(name.toUpperCase());
+//		System.out.println(name.length());
+		
+//		int[] num = {1,2,3,4,9};
+//		for(int i=0; i < num.length ; i++)
+//			System.out.println(num[i]);
+		
+//		int[] num2 = new int[100];
+		
+//		String[] fruits = {"banana","apple"};
+//		for(String f : fruits)
+//			System.out.println(f);
+		String tel = "053-211-222";
+		String[] ts = tel.split("-");
+		for(String t : ts)
+			System.out.println(t);
+		
+	}
+}
